5eb2bb14948c0be132fb612eb2d786e1b988f22d
pauladelavictoria <pauladelavictoria@gmail.com>
2021-12-31 19:09:38 +0100

    css changes and id for favourites

--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,77 @@
-'use strict';
+"use strict";
 
 // constantes
-const searchInput = document.querySelector('.js-input');
-const searchBtn = document.querySelector('.js-searchBtn');
-const serieList = document.querySelector('.js-serieList');
+const searchInput = document.querySelector(".js-input");
+const searchBtn = document.querySelector(".js-searchBtn");
+const serieList = document.querySelector(".js-serieList");
+const mainContainer = document.querySelector(".main");
 
 // Evento y función para buscar las series
-searchBtn.addEventListener('click', searchSerie);
+searchBtn.addEventListener("click", searchSerie);
 
 function searchSerie() {
+  
   fetch(`https://api.jikan.moe/v3/search/anime?q=${searchInput.value}`)
     .then((response) => response.json())
     .then((data) => {
-
       // arrSeries se cambia por data.result para darle ese valor al parámetro que tiene la función
-      paintSeries(data.results);
+      // Condicional por si no existe en la API la serie que se busca
+      if (data.results.length > 0) {
+        paintSeries(data.results);
+      } else {
+        const noList = document.createElement("p");
+        noList.classList.add("notFound");
+        const noListText = document.createTextNode(
+          "No tenemos la serie que buscas, prueba otra 🙈"
+        );
+        noList.appendChild(noListText);
+        mainContainer.appendChild(noList);
+      }
     });
 }
 
 // función para pintar cada serie
 function paintSeries(arrSeries) {
   for (const eachSerie of arrSeries) {
-    // crear el li, div y texto
-    const listEl = document.createElement('li');
-    const container = document.createElement('div');
-    const listText = document.createTextNode(eachSerie.title); 
+    // crear el li, div, e id
+    const listEl = document.createElement("li");
+    const container = document.createElement("div");
+    listEl.setAttribute("id", eachSerie.mal_id);
     // Crear la imagen si no fuese background
     // const listImg = document.createElement('img');
     // listImg.setAttribute('src', eachSerie.image_url);
     // listEl.appendChild(listImg);
     // listImg.classList.add('listImg');
 
-    // Imagen de fondo
-    listEl.style.backgroundImage = `url(${eachSerie.image_url})`;
+    // Imagen de fondo y texto
+    if (eachSerie.image_url) {
+      listEl.style.backgroundImage = `url(${eachSerie.image_url})`;
+      const listText = document.createTextNode(eachSerie.title);
+      container.appendChild(listText);
+      container.classList.add("divText");
+    } else {
+      listEl.style.backgroundImage = `url(https://via.placeholder.com/400x400/f96457/ffffff/?text=${eachSerie.title})`;
+    }
 
     // El ul tiene un li, con un contenedor y el texto dentro del contenedor div
     serieList.appendChild(listEl);
     listEl.appendChild(container);
-    container.appendChild(listText);
-    
-    // clase para cambiar el li y container en css
-    listEl.classList.add('listEl');
-    container.classList.add('divText');
+
+    // clase para cambiar el li en css
+    listEl.classList.add("listEl");
+
+    // Evento para añadir a favorito
+    listEl.addEventListener("click", addFav);
   }
+}
+
+// LOCALSTORAGE
+
+// Array vacío donde se van a guardar los id de los fav
+let idFav = [];
+
+// función para guardar como fav
+function addFav(event) {
+  idFav.push(event.currentTarget.id);
+  localStorage.setItem("idFav", JSON.stringify(idFav));
 }
